57db2786fe4ecc2cf1eb3fdd40d5c8e46fa82f51
Cesar Guerra <cesargb504@gmail.com>
2016-11-25 00:33:24 -0800

    generador codigo declaraciones

--- ejemplos/main.m
+++ ejemplos/main.m
@@ -1,23 +1,40 @@
+mainBloque:
+mem asd 64
+s
 t_0 =  78.12 * 234
+s
 t_1 =  45 + t_0
+s
 t_2 =  t_1 + 56
 asd = t_2
+mem asd2 32
 asd2 = 7
+mem b1 8
 b1 = true
+mem b2 8
+s
 t_3 =  true && b1
+s
 t_4 =  false || t_3
 b2 = t_4
 asd = 342
-asd = 123.43
-b2 = false
-asd = 0
-b2 = true
-goto end_if5
-nif5:
+s
 t_5 =  b1 == true
 if t_5==false goto nif6
-t_6 =  asd + 1
-asd = t_6
-mainBloque:
-mainBloque end
+asd = 123.43
+b2 = false
+goto end_if6
+nif6:
+asd = 0
+b2 = true
+s
+t_6 =  b1 == true
+t_6
+cloop7:
+if t_6==false goto end_cloop7
+s
+t_8 =  asd + 1
+asd = t_8
+end_cloop7
 end_if6
+mainBloque end
--- mcodegen.rb
+++ mcodegen.rb
@@ -1,42 +1,39 @@
-
 class MCodeGen
-    def initialize tree     
+    def initialize tree
         @tree=tree
         @fIni=[]
         @fEnd=[]
         @t=0
-        
-    end
-    def nextNode
-        @tree.each do |item|
-            if item.children.count==0 
-                n= item.dclone
-                p=item.parent
-                p.remove!(item)
-                return n
-            end
-        end    
-    end    
-    def genFile 
-        File.open("./ejemplos/main.m","w") do |f| 
-        
-            @tree.postordered_each do |item|
-                begin
-                    
-                    send "pr_"+item.name[/ .+ /].to_s.strip,item
-                rescue NoMethodError 
-                end
-               
-            
-               
-            
-            
+        @nextNode=nil
 
-            end 
+    end
+    def correctNode? node
+        node.children.each { |e| return false if !e.b   }
+        true
+    end
+
+
+    def genFile
+        File.open("./ejemplos/main.m","w") do |f|
+
+            @tree.each do |item|
+                puts item.name
+                begin
+
+                    send "pr_"+item.name[/ .+ /].to_s.strip,item
+                rescue NoMethodError
+                end
+
+
+
+
+
+
+            end
             @fIni.each { |i|  f.puts  i }
             @fEnd.reverse_each{ |i|  f.puts  i }
       end
-        
+
     end
 
     def node_to_array node
@@ -46,9 +43,11 @@ class MCodeGen
     end
 
     def pr_mainBloque node
-        
+
         @fIni<<"mainBloque:"
         @fEnd<<"mainBloque end"
+       #@fIni.unshift "mainBloque:"
+       #@fIni << "mainBloque end"
     end
 
     def pr_declaracion node
@@ -59,10 +58,29 @@ class MCodeGen
         puts
         array.shift
         result=nil
-    
-        array = PostFix.infix_to_posfix  array 
+
+        array = PostFix.infix_to_posfix  array
+        dec="mem "
+        puts "mem"
+        puts node.children
+        dec+=node[1].content.val
+        dec+=" "
+
+        case node[3].content.val
+        when "num"
+          dec+="32"
+        when "float"
+          dec+="32"
+        when "real"
+          dec+="64"
+        when "bool"
+          dec+="8"
+        when "string"
+          dec+="16"
+        end
+        @fIni<<dec
         print_pfix array , node[1]
-        puts 
+        puts
     end
 
     def pr_estPregunta node
@@ -75,17 +93,17 @@ class MCodeGen
     def pr_estNif node
         @fIni<<"goto end_if#{@t}"
         @fIni<< "nif#{@t}:"
-        
+
         puts
     end
     def pr_asignar node
-        puts node.children
+        #puts node.children
         array = node_to_array  node[1]
-        puts array
+        #puts array
         array.shift
         result=nil
-    
-        array = PostFix.infix_to_posfix  array 
+
+        array = PostFix.infix_to_posfix  array
         print_pfix array , node[0]
         puts
     end
@@ -93,41 +111,53 @@ class MCodeGen
         array =PostFix.infix_to_posfix node_to_array(node.children[1])
         var= print_pfix array
         @fIni<<var
-        @fIni<<"cloop#{@t}: /////////"
-
+        @fIni<<"cloop#{@t}:"
+        @fIni<< "if #{var}==false goto end_cloop#{@t}"
+        @fEnd<<"end_cloop#{@t}"
+        @t+=1
     end
-    
+
 
     def instrucciones_to_array node
         i=0
         tmp = node
         array=[]
-        puts tmp[i]
+        #puts tmp[i]
         while i<tmp.children.count
             if tmp.children[i].name[/ .+ /].to_s.strip == "instruccion"
-                array<<tmp.children[i] 
+                array<<tmp.children[i]
                 i+=1
             elsif tmp.children[i].name[/ .+ /].to_s.strip == "instrucciones"
                 tmp=tmp.children[i]
                 i==0
-            end 
+            end
         end
         array
     end
 
-    
+
     def print_pfix tokens , resultToken=nil
         i=0
         File.open("./ejemplos/main.m","a") do |f|
+
             if tokens.count==1
-                @fIni<< "#{resultToken.content.val} = #{tokens[0].val}"
+                if resultToken
+
+                    @fIni<< "#{resultToken.content.val} = #{tokens[0].val}"
+                else
+
+                    @fIni<< "t_#{@t} = #{tokens[0].val}"
+                    @t+=1
+                    return "t_#{@t}"
+                end
                 return
             end
             while i<tokens.count
-                case tokens[i].val 
+                case tokens[i].val
                 when "+","*","-",">","<",">=","<=","==","!=","&&","||"
+                    @fIni<<"s"
                     @fIni<< "t_#{@t} =  #{tokens[i-2].val} #{tokens[i].val} #{tokens[i-1].val}"
-                    
+
                     tokens.delete_at(i-2)
                     tokens.delete_at(i-2)
                     tokens[i-2].val="t_"+@t.to_s
@@ -137,7 +167,7 @@ class MCodeGen
                 i+=1
             end
             if resultToken
-                @fIni<< "#{resultToken.content.val} = t_#{@t-1}" 
+                @fIni<< "#{resultToken.content.val} = t_#{@t-1}"
                 return resultToken
             end
             "t_#{@t-1}"
